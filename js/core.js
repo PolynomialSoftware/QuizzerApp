0cc2da83ee5dfe3abc3c9fa5844d448eadecd302
Timon Chu <tchu6@users.noreply.github.com>
2017-10-19 15:54:21 -0600

    Added random question functionality

--- js/core.js
+++ js/core.js
@@ -8,11 +8,17 @@ $(document).ready(function () {
   var quiz = JSON.parse(text);
 
   // Display total number of Questions
+  $("#totalQuizQuestions").text(quiz.questions.length);
 
   // Display current score
+  
+
+  // Generate random question number 
+  var randomQuestionNum = Math.floor(Math.random() * quiz.questions.length); 
+
 
   // Display One Question
-  displayOneQuestion(quiz);
+  displayOneQuestion(quiz, randomQuestionNum);
 })
 
 $("#nextButton").click(function () {
@@ -20,9 +26,9 @@ $("#nextButton").click(function () {
   
 })
 
-function displayOneQuestion(quiz)
+function displayOneQuestion(quiz, randomQuestionNum)
 {
-  var questionIndex = 5;
+  var questionIndex = randomQuestionNum;
   $("#options").empty();    // Clear old options
   $("#question").text(quiz.questions[questionIndex].question);
   for (i = 0; i < quiz.questions[questionIndex].options.length; i++)
